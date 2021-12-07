f2912069457de2bf227e4329dd90be50ccf3d301
cavilpa <carlosavilpa@gmail.com>
2021-12-07 11:18:40 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jugador de Basket</title>
+</head>
+<body>
+    <?php
+        include_once './player.php';
+
+        $player1 = new player();
+
+        $player1->informacion();
+
+
+    ?>
+</body>
+</html>
